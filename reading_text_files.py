ec17b2fa09e340ea0b0cf980bdc6e9ecf3a72ef5
Chinaza Igbokwe <98115827+buenodice@users.noreply.github.com>
2022-05-28 22:15:41 +0100

    Add files via upload

---
+++ reading_text_files.py
@@ -0,0 +1,24 @@
+# read  text from a file, and count the occurence of words in that text
+# Examples:
+# count words("The cake is done. It is a big cake")
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it:1"}
+
+def read_file_content(filename) :
+    # [assignment] Add your code here
+    with open("story.txt", "r") as d:
+      file = d.read()
+      return file
+
+
+def count_words():
+    text = read_file_content("story.txt")
+    # [assignment] Add your code here
+    count = dict()
+    words = text.split()
+    for word in words:
+        if word in count:
+            count[word] = +1
+        else:
+            count[word] = 1
+    return (count)
+print (count_words())
